34b7bd469ca241e503a8028b494b60c558795dfc
Joaquim Lucas Pessoa da Silva <joaquim.pessoa@tutamail.com>
2025-01-30 12:36:43 -0300

    Update in vectors.js

--- vectors.js
+++ vectors.js
@@ -3,3 +3,15 @@ const vector = Array();
 vector.push(10, 20, 30);
 
 console.log(vector);
+
+for (let a of vector)   {
+    console.log(a);
+}
+
+let sum = 0;
+
+for (let count = 0; count < vector.length; count++) {
+    sum += vector[count];
+}
+
+console.log(sum);
