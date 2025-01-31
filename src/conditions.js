23d114eb45e1326c2ecbad2412ca3b1def0bba2e
Joaquim Lucas Pessoa da Silva <joaquim.pessoa@tutamail.com>
2025-01-31 17:34:30 -0300

    Recursive function prime for conditionals.js

--- src/conditions.js
+++ src/conditions.js
@@ -7,3 +7,16 @@ if ((a === 'M' && b >= 65 && c >= 25) ^ (a === 'F' && b >= 62 && c >= 20))  {
 }   else    {
     console.log("Keep working.");
 }
+
+function prime (x, y = 2)   {
+    if (x <= 1 ^ (x % y === 0 && x !== y)) {
+        return false;
+    }   else if (x === y)   {
+        return true;
+    }   else    {
+        return prime(x, y + 1);
+    }
+}
+
+console.log(prime(23));
+
