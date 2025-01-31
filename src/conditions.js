61a6f2e6db1cabd2fbaab4b1725f7baed04f91c7
Joaquim Lucas Pessoa da Silva <joaquim.pessoa@tutamail.com>
2025-01-31 17:42:53 -0300

    Iterative function prime for conditionals.js

--- src/conditions.js
+++ src/conditions.js
@@ -7,3 +7,26 @@ if ((a === 'M' && b >= 65 && c >= 25) ^ (a === 'F' && b >= 62 && c >= 20))  {
 }   else    {
     console.log("Keep working.");
 }
+
+function prime (x)  {
+    let verify;
+    let divider;
+
+    if (x <= 1) {
+        return false;
+    }   else    {
+        divider = 2;
+        verify = true;
+
+        while (divider < x && verify)   {
+            if (x % divider === 0)  {
+                verify = false;
+            }   else    {
+                ++divider;
+            }
+        }
+        return verify;
+    }
+}
+
+console.log(prime(23));
